dab8e5ca6921444ec60728f4cdeec14914410535
Максим Спицын <mgraf@yandex.ru>
2022-05-31 13:17:00 +0300

    добавил модели Title, Genre, Category и пустые Review, Comments

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Категории (типы: фильм, книга или песенка) произведений"""
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Genre(models.Model):
+    """Категории жанров"""
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Title(models.Model):
+    """Произведения, к которым пишут отзывы
+    (определённый фильм, книга или песенка)"""
+    name = models.TextField()
+    year = models.IntegerField()
+    сategory = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        related_name="сategory", blank=True, null=True
+    )
+    genres = models.ManyToManyField(
+        Genre, on_delete=models.SET_NULL,
+        related_name="genres", blank=True, null=True
+    )
+    rating = models.IntegerField(
+        null=True,
+        default=None
+    )
+
+    def __str__(self):
+        return self.text
+
+
+class Review(models.Model):
+    """Отзывы на произведения (Title)"""
+    pass
+    # делает Кирилл
+
+
+class Comments(models.Model):
+    """Комментарии к отзывам (Review)"""
+    pass
+    # делает Кирилл
