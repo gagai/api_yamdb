5198ce05561100f5c571512ab91bf8a38869b992
Максим Спицын <mgraf@yandex.ru>
2022-05-31 22:18:10 +0300

    установил в settings permissions.IsAuthenticatedOrReadOnly

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -37,7 +37,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         title_id = self.kwargs.get('title_id')
         title = get_object_or_404(Review, title_id=title_id)
-        return title.reviews.all() 
+        return title.reviews.all()
 
 
 class CommentsViewSet(viewsets.ModelViewSet):
