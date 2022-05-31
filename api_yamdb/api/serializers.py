32c118879570013dfe6671f2360323da4450faf6
Максим Спицын <mgraf@yandex.ru>
2022-05-31 16:57:23 +0300

    сделал структуру проекта. Добавил в api serializers,views,urls

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from reviews.models import User, Category, Genre, Title, Review, Comment
+
+
+class UserSerializer(serializers.ModelSerializer):
+    pass
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Category
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Genre
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Title
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    pass
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    pass
