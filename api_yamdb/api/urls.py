d6c935491cb5f640fd8babe178d86f20b5dd4e97
Kirill Kudryashov <b1nanon@mail.ru>
2022-06-01 17:23:16 +0300

    Добавил App_name

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import (UserViewSet, TitleViewSet, CategoryViewSet, GenreViewSet,
                     ReviewViewSet, CommentViewSet)
+app_name = 'api'
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet)
